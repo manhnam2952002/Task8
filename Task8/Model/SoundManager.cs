f96b81c11b48040117f01459f0d871f30b3e8fc0
Manh Nam <Manh Nam@DESKTOP-4PD0VRB>
2021-10-11 17:09:24 +0700

    Add project files.

---
+++ Task8/Model/SoundManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8.Model
+{
+    public class SoundManager
+    {
+       public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSound = GetSounds();
+            sounds.Clear();
+            allSound.ForEach(p => sounds.Add(p));
+        }
+        
+        
+        public static void GetSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory soundCategory)
+        {
+            var allSound = GetSounds();
+            var filteredSounds = allSound.Where(p => p.Category == soundCategory).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(p => sounds.Add(p));
+        } 
+
+        private static List<Sound> GetSounds()
+        {
+            var sounds = new List<Sound>();
+
+            sounds.Add(new Sound("Cow", SoundCategory.Animals));
+            sounds.Add(new Sound("Cat", SoundCategory.Animals));
+
+            sounds.Add(new Sound("Gun", SoundCategory.Cartoons));
+            sounds.Add(new Sound("Spring", SoundCategory.Cartoons));
+
+            sounds.Add(new Sound("Clock", SoundCategory.Taunts));
+            sounds.Add(new Sound("LOL", SoundCategory.Taunts));
+
+            sounds.Add(new Sound("Ship", SoundCategory.Warnings));
+            sounds.Add(new Sound("Siren", SoundCategory.Warnings));
+
+            return sounds;
+        }
+
+    }
+}
